cbe066257662c25e635226da7f4b34460520a6cd
BhavyaSreeJallolla <bhavyasreejalolla967@gmail.com>
2026-07-24 23:45:06 +0530

    frond url added

--- CampusConnect/src/main/java/com/campusconnect/config/SecurityConfig.java
+++ CampusConnect/src/main/java/com/campusconnect/config/SecurityConfig.java
@@ -280,11 +280,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(
                 List.of(
                     "http://localhost:5173",
-                    "http://localhost:5174",
-                    "http://localhost:5175",
-                    "http://localhost:5177",
-                    "http://localhost:5178",
-                    "http://localhost:5179"
+                    "https://nextgen-campus.netlify.app/"
                 )
         );
 
